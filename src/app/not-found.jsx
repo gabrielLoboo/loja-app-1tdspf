fcf2af05c319a5dfdcb38e8ec4665b89e5aa4116
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-10-19 11:46:19 -0300

    Estilizando o footer e navbar com tailwind

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+
 export default function Notfound() {
   return (
     <div>
         <h1>AOBA 404!</h1>
+        <div>
+          <figure>
+            <Image src="http://http.cat/images/404.jpg" alt="Gato no jornal" width={700} height={600}></Image>
+          </figure>
+        </div>
         <p>Página não encontrada</p>
     </div>
   )
