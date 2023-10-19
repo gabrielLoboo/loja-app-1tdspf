e062039b97e709396dec3e67320ada5ba64923dd
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-10-19 11:45:47 -0300

    Rodape criado

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,10 @@
+export default function Rodape() {
+  return (
+    <footer className="rodape">
+        <div>
+            <p>Desenvolvido por <span>RM99708</span></p>
+            <p>NEXT.js - React.js - Tailwind</p>
+        </div>
+    </footer>
+  )
+}
