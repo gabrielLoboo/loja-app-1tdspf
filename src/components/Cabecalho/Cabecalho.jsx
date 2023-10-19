2ef6efb38a6ac04cb0aa1ab9775bc1a0c1ee5931
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-10-19 11:45:59 -0300

    Cabecalho criado

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Cabecalho() {
+  return (
+    <header className="cabecalho">
+            <h1>Meus Produtos</h1>
+              <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="./produtos/tenis">Tenis</Link></li>
+                <li><Link href="./produtos/calca">Calça</Link></li>
+                <li><Link href="./produtos/camisa">Camisa</Link></li>
+                <li><Link href="./produtos/meia">Meia</Link></li>
+              </ul>
+        </header>
+  )
+}
